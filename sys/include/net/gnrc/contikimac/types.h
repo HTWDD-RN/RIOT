6cc6711c624d1791683c4dea42a26446d47620f0
Alexander Gräb <mail@hpvg.de>
2017-10-14 11:50:01 +0200

    Code gesaeubert. => Nur noch prototypische Implementierung der CCA-Phase enthalten.

--- sys/include/net/gnrc/contikimac/types.h
+++ sys/include/net/gnrc/contikimac/types.h
@@ -25,81 +25,6 @@ extern "C" {
 #define GNRC_CONTIKIMAC_EVENT_RTT_PACKET_DETECTED  (0x4302)
 
 /**
- * @brief   CONTIKIMAC send strobe packet.
- */
-#define GNRC_CONTIKIMAC_EVENT_SND_TYPE  (0x4400)
-
-///**
-// * @brief   CONTIKIMAC RTT start event type.
-// */
-//#define GNRC_CONTIKIMAC_EVENT_RTT_START           (0x4301)
-//
-///**
-// * @brief   CONTIKIMAC RTT stop event type.
-// */
-//#define GNRC_CONTIKIMAC_EVENT_RTT_STOP            (0x4302)
-//
-///**
-// * @brief   CONTIKIMAC RTT pause event type.
-// */
-//#define GNRC_CONTIKIMAC_EVENT_RTT_PAUSE           (0x4303)
-//
-///**
-// * @brief   CONTIKIMAC RTT resume event type.
-// */
-//#define GNRC_CONTIKIMAC_EVENT_RTT_RESUME          (0x4304)
-//
-///**
-// * @brief   CONTIKIMAC RTT wakeup pending event type.
-// */
-//#define GNRC_CONTIKIMAC_EVENT_RTT_WAKEUP_PENDING  (0x4305)
-//
-///**
-// * @brief   CONTIKIMAC RTT sleep pending event type.
-// */
-//#define GNRC_CONTIKIMAC_EVENT_RTT_SLEEP_PENDING   (0x4306)
-//
-///**
-// * @brief   CONTIKIMAC timeout event type.
-// */
-//#define GNRC_CONTIKIMAC_EVENT_TIMEOUT_TYPE        (0x4400)
-//
-///**
-// * @brief   CONTIKIMAC duty-cycle active flag.
-// *
-// * Keep track of duty cycling to avoid late RTT events after stopping.
-// */
-//#define GNRC_CONTIKIMAC_DUTYCYCLE_ACTIVE          (0x01)
-//
-///**
-// * @brief   CONTIKIMAC needs reschedule flag.
-// *
-// * Used internally for rescheduling state machine update, e.g. after state
-// * transition caused in update.
-// */
-//#define GNRC_CONTIKIMAC_NEEDS_RESCHEDULE          (0x02)
-//
-///**
-// * @brief   CONTIKIMAC check radio's on/off state flag.
-// */
-//#define GNRC_CONTIKIMAC_RADIO_IS_ON               (0x04)
-//
-///**
-// * @brief   Enable/disable duty-cycle record and print out.
-// *          Set "1" to enable, set "0" to disable.
-// */
-//#ifndef GNRC_CONTIKIMAC_ENABLE_DUTYCYLE_RECORD
-//#define GNRC_CONTIKIMAC_ENABLE_DUTYCYLE_RECORD    (0U)
-//#endif
-//
-///**
-// * @brief The default largest number of parallel timeouts in CONTIKIMAC
-// */
-//#ifndef GNRC_CONTIKIMAC_TIMEOUT_COUNT
-//#define GNRC_CONTIKIMAC_TIMEOUT_COUNT             (3U)
-//#endif
-
-/**
  * @brief   Internal states of CONTIKIMAC
  */
 typedef enum {
@@ -115,80 +40,6 @@ typedef enum {
     GNRC_CONTIKIMAC_STATE_COUNT     /**< Count of CONTIKIMAC's states */
 } gnrc_contikimac_state_t;
 
-///**
-// * @brief   TX states of CONTIKIMAC
-// */
-//typedef enum {
-//    GNRC_CONTIKIMAC_TX_STATE_STOPPED,           /**< Tx schedule stopped, stop sending packet */
-//    GNRC_CONTIKIMAC_TX_STATE_INIT,              /**< Initiate transmission */
-//    GNRC_CONTIKIMAC_TX_STATE_SEND_BROADCAST,    /**< directly goes to SUCCESSFUL or FAILED when finished */
-//    GNRC_CONTIKIMAC_TX_STATE_SEND_WR,           /**< Send a wakeup request */
-//    GNRC_CONTIKIMAC_TX_STATE_WAIT_WR_SENT,      /**< Wait until WR sent to set timeout */
-//    GNRC_CONTIKIMAC_TX_STATE_WAIT_FOR_WA,       /**< Wait for dest node's wakeup ackknowledge */
-//    GNRC_CONTIKIMAC_TX_STATE_SEND_DATA,         /**< Send the actual payload data */
-//    GNRC_CONTIKIMAC_TX_STATE_WAIT_FEEDBACK,     /**< Wait if packet was ACKed */
-//    GNRC_CONTIKIMAC_TX_STATE_SUCCESSFUL,        /**< Transmission has finished successfully */
-//    GNRC_CONTIKIMAC_TX_STATE_FAILED             /**< Payload data couldn't be delivered to dest */
-//} gnrc_contikimac_tx_state_t;
-//
-///**
-// * @brief   Static initializer for gnrc_contikimac_tx_state_t.
-// */
-//#define GNRC_CONTIKIMAC_TX_STATE_INITIAL GNRC_CONTIKIMAC_TX_STATE_STOPPED
-//
-///**
-// * @brief   RX states of CONTIKIMAC
-// */
-//typedef enum {
-//    GNRC_CONTIKIMAC_RX_STATE_STOPPED,       /**< Rx schedule stopped */
-//    GNRC_CONTIKIMAC_RX_STATE_INIT,          /**< Initiate reception */
-//    GNRC_CONTIKIMAC_RX_STATE_WAIT_FOR_WR,   /**< Wait for a wakeup request */
-//    GNRC_CONTIKIMAC_RX_STATE_SEND_WA,       /**< Send wakeup ackknowledge to requesting node */
-//    GNRC_CONTIKIMAC_RX_STATE_WAIT_WA_SENT,  /**< Wait until WA sent to set timeout */
-//    GNRC_CONTIKIMAC_RX_STATE_WAIT_FOR_DATA, /**< Wait for actual payload data */
-//    GNRC_CONTIKIMAC_RX_STATE_SUCCESSFUL,    /**< Recption has finished successfully */
-//    GNRC_CONTIKIMAC_RX_STATE_FAILED         /**< Reception over, but nothing received */
-//} gnrc_contikimac_rx_state_t;
-//
-///**
-// * @brief   Static initializer for gnrc_contikimac_rx_state_t.
-// */
-//#define GNRC_CONTIKIMAC_RX_STATE_INITIAL GNRC_CONTIKIMAC_RX_STATE_STOPPED
-//
-///**
-// * @brief   CONTIKIMAC uninitialized phase value
-// */
-//#define GNRC_CONTIKIMAC_PHASE_UNINITIALIZED   (0)
-//
-///**
-// * @brief   CONTIKIMAC max phase value
-// */
-//#define GNRC_CONTIKIMAC_PHASE_MAX             (-1)
-
-/**
- * @brief   CONTIKIMAC xtimer types
- */
-typedef enum {
-    GNRC_CONTIKIMAC_XTIMER_SEND_STROBE,              /**< Send packet that belongs to a strobe */
-//    GNRC_CONTIKIMAC_TIMEOUT_WR,                    /**< WR timeout, waiting WA */
-//    GNRC_CONTIKIMAC_TIMEOUT_NO_RESPONSE,           /**< Maximum WR duration timeout awaiting WA */
-//    GNRC_CONTIKIMAC_TIMEOUT_DATA,                  /**< Timeout awaiting data packet from receiver */
-//    GNRC_CONTIKIMAC_TIMEOUT_WAIT_DEST_WAKEUP,      /**< Timeout for waiting receiver's wake-up phase */
-//    GNRC_CONTIKIMAC_TIMEOUT_WAKEUP_PERIOD,         /**< Wake up period timeout for going to sleep */
-//    GNRC_CONTIKIMAC_TIMEOUT_NEXT_BROADCAST,        /**< Timeout for waiting to send the next broadcast packet */
-//    GNRC_CONTIKIMAC_TIMEOUT_BROADCAST_END,         /**< Timeout awaiting the end of the whole broadcast period */
-} gnrc_contikimac_xtimer_type_t;
-
-///**
-// * @brief   CONTIKIMAC timeout structure
-// */
-//typedef struct {
-//    xtimer_t timer;                 /**< xtimer entity */
-//    msg_t msg;                      /**< msg entity */
-//    bool expired;                   /**< If type != DISABLED, this indicates if timeout has expired */
-//    gnrc_contikimac_timeout_type_t type; /**< timeout type */
-//} gnrc_contikimac_timeout_t;
-
 /**
  * @brief   CONTIKIMAC specific structure for storing internal states.
  */
@@ -197,17 +48,6 @@ typedef struct contikimac {
     xtimer_ticks32_t strobe_start;							/** Time when the current strobe was started */
     uint32_t last_wakeup;                                    /**< Used to calculate wakeup times */
     uint8_t contikimac_info;                                      /**< CONTIKIMAC's internal informations (flags) */
-//    gnrc_contikimac_timeout_t timeouts[GNRC_CONTIKIMAC_TIMEOUT_COUNT]; /**< Store timeouts used for protocol */
-
-//#if (GNRC_CONTIKIMAC_ENABLE_DUTYCYLE_RECORD == 1)
-//    /* Parameters for recording duty-cycle */
-//    uint32_t last_radio_on_time_ticks;                       /**< The last time in ticks when radio is on */
-//    uint32_t radio_off_time_ticks;                           /**< The time in ticks when radio is off */
-//    uint32_t system_start_time_ticks;                        /**< The time in ticks when chip is started */
-//    uint32_t awake_duration_sum_ticks;                       /**< The sum of time in ticks when radio is on */
-//    uint32_t pkt_start_sending_time_ticks;                   /**< The time in ticks when the packet is started
-//                                                                  to be sent */
-//#endif
 } gnrc_contikimac_t;
 
 #ifdef __cplusplus
--- sys/net/gnrc/link_layer/contikimac/contikimac.c
+++ sys/net/gnrc/link_layer/contikimac/contikimac.c
@@ -22,117 +22,11 @@ bool packet_received;
 bool sending_broadcast;
 xtimer_t xtimer;
 
-static bool contikimac_send(gnrc_netdev_t *netdev, gnrc_pktsnip_t *pkt);
 static void contikimac_set_netdev_state(gnrc_netdev_t *gnrc_netdev, netopt_state_t devstate);
 static bool contikimac_perform_cca_phase(netdev_t *dev);
 static void _pass_on_packet(gnrc_pktsnip_t *pkt);
-static void contikimac_set_state(gnrc_netdev_t *gnrc_netdev, gnrc_contikimac_state_t newstate);
 static void rtt_cb(void *arg);
 static void rtt_handler(uint32_t event, gnrc_netdev_t *gnrc_netdev);
-
-/**
- * @brief				ContikiMAC's send function.
- *
- * @param[in] netdev	Pointer to the underlying netdev device.
- *
- * @return          	True, if the packet was successful transmitted.
- */
-//bool contikimac_send(gnrc_netdev_t *netdev, gnrc_pktsnip_t *pkt)
-//{
-//	//netdev->send(netdev, pkt);
-//	//return true;
-//	
-//	xtimer_ticks32_t transmission_start, now;
-//	//int status;
-//
-//	netdev->contikimac.state = GNRC_CONTIKIMAC_TRANSMITTING;
-//	transmission_start = xtimer_now();
-//	contikimac_set_netdev_state(netdev, NETOPT_STATE_IDLE);
-//	
-////	/* Disable Auto ACK */
-////	netopt_enable_t autoack = NETOPT_DISABLE;
-////	netdev->dev->driver->set(netdev->dev, NETOPT_AUTOACK, &autoack, sizeof(autoack));
-//	
-//	packet_received = false;
-//	DEBUG("[CONTIKIMAC] Sending packet!\n");
-//	
-//	do {
-//		/* Don't let the packet be released yet, we want to send it again */
-//		gnrc_pktbuf_hold(pkt, 1);
-//		
-//		/*status = */netdev->send(netdev, pkt);
-//		now = xtimer_now();
-//		xtimer_periodic_wakeup(&now, GNRC_CONTIKIMAC_INTER_PACKET_INTERVAL_US);
-//		now = xtimer_now();
-//		//DEBUG("[CONTIKIMAC] Packet sent! %i\n", status);
-//		
-//	} while(!packet_received && ((now.ticks32 - transmission_start.ticks32) < GNRC_CONTIKIMAC_STROBE_TIME));
-//	
-//	gnrc_pktbuf_release(pkt);
-////	contikimac_set_netdev_state(netdev, NETOPT_STATE_SLEEP);
-//	netdev->contikimac.state = GNRC_CONTIKIMAC_SLEEPING;
-//	
-//	DEBUG("[CONTIKIMAC] Sending packet done!\n");
-//	
-////	/* Enable Auto again */
-////	autoack = NETOPT_ENABLE;
-////	netdev->dev->driver->set(netdev->dev, NETOPT_AUTOACK, &autoack, sizeof(autoack));
-//	
-//	return true;
-//}
-//
-// Alle 25666 us, erster Aufruf dauert 10544 us, alle weiteren ~6838 us. 25656 us zwischen den Aufrufen.
-bool contikimac_send(gnrc_netdev_t *netdev, gnrc_pktsnip_t *pkt)
-{
-	static msg_t msg;
-	static xtimer_ticks32_t now1;
-	
-	/* We are not already transmitting, so it must be the first strobe packet. */
-	if (netdev->contikimac.state != GNRC_CONTIKIMAC_TRANSMITTING) {
-		netdev->contikimac.state = GNRC_CONTIKIMAC_TRANSMITTING;
-		
-		/* check if the packet is for broadcast */
-		if (gnrc_netif_hdr_get_flag(pkt) & (GNRC_NETIF_HDR_FLAGS_BROADCAST | GNRC_NETIF_HDR_FLAGS_MULTICAST)) {
-			DEBUG("[CONTIKIMAC] We've got a broadcast packet.\n");
-			sending_broadcast = true;
-		} else {
-			DEBUG("[CONTIKIMAC] We've got a unicast packet.\n");
-			sending_broadcast = false;
-		}
-		
-		netdev->contikimac.strobe_start = xtimer_now();
-		packet_received = false;
-	} else { // DEBUG!
-		xtimer_ticks32_t now2 = xtimer_now();
-		DEBUG("[CONTIKIMAC] contikimac_send() %lu us between calls.\n", now2.ticks32 - now1.ticks32);
-	}
-	
-	gnrc_pktbuf_hold(pkt, 1);
-	
-	//xtimer_ticks32_t now = xtimer_now();
-	netdev->send(netdev, pkt); // 109 Bytes => 6803 us
-	//xtimer_ticks32_t now2 = xtimer_now();
-	//DEBUG("[CONTIKIMAC] Sending packet done! %lu\n", now2.ticks32 - now.ticks32); // -> 6803 us
-	
-	xtimer_ticks32_t now = xtimer_now();
-	
-	/* No ACK was received so far und strobe is not over now. Schedule the next packet. */
-	if ((!packet_received || sending_broadcast) && ((now.ticks32 - netdev->contikimac.strobe_start.ticks32) < GNRC_CONTIKIMAC_STROBE_TIME)) {
-		msg.type = GNRC_CONTIKIMAC_EVENT_SND_TYPE;
-		msg.content.ptr = pkt;
-		//msg_send(&msg, contikimac_pid);
-		//msg_send_to_self(&msg);
-		
-		xtimer_set_msg(&xtimer, 50000 /*GNRC_CONTIKIMAC_INTER_PACKET_INTERVAL_US*/, &msg, contikimac_pid);
-	} else {
-		gnrc_pktbuf_release(pkt);
-		netdev->contikimac.state = 0; // TODO: Set the right state.
-	}
-	
-	now1 = xtimer_now(); // DEBUG!
-	
-	return true;
-}
 
 void contikimac_set_netdev_state(gnrc_netdev_t *gnrc_netdev, netopt_state_t devstate)
 {
@@ -159,7 +53,11 @@ bool contikimac_perform_cca_phase(netdev_t *dev)
 			xtimer_periodic_wakeup(&last_wakeup, GNRC_CONTIKIMAC_CCA_SLEEP_TIME_US);
 		}
 
+		/* Uncomment the two LED0-1 lines below, if you want to measure the time a cca call takes on pin PA19 (LED0 pin, board samr21-xpro).
+		 * Remember to comment out all other LED0-* lines. Note that LED0 is low active, that means, switching the LED off let become pin PA19 high. */
+		//LED0_OFF; // LED0-1
 		dev->driver->get(dev, NETOPT_IS_CHANNEL_CLR, &channel_free, sizeof(channel_free));
+		//LED0_ON; // LED0-1
 
 		if (!channel_free) return true;
 	}
@@ -167,123 +65,10 @@ bool contikimac_perform_cca_phase(netdev_t *dev)
 	return false;
 }
 
-void contikimac_set_state(gnrc_netdev_t *gnrc_netdev, gnrc_contikimac_state_t newstate)
-{
-//    gnrc_contikimac_state_t oldstate = gnrc_netdev->lwmac.state;
-//
-//    if (newstate == oldstate) {
-//        return;
-//    }
-//
-//    if (newstate >= GNRC_LWMAC_STATE_COUNT) {
-//        LOG_ERROR("ERROR: [LWMAC] Trying to set invalid state %u\n", newstate);
-//        return;
-//    }
-//
-//    /* Already change state, but might be reverted to oldstate when needed */
-//    gnrc_netdev->lwmac.state = newstate;
-//
-//    /* Actions when leaving old state */
-//    switch (oldstate) {
-//        case GNRC_LWMAC_RECEIVING:
-//        case GNRC_LWMAC_TRANSMITTING: {
-//            /* Enable duty cycling again */
-//            rtt_handler(GNRC_LWMAC_EVENT_RTT_RESUME, gnrc_netdev);
-//#if (GNRC_LWMAC_ENABLE_DUTYCYLE_RECORD == 1)
-//            /* Output duty-cycle ratio */
-//            uint64_t duty;
-//            duty = (uint64_t) rtt_get_counter();
-//            duty = ((uint64_t) gnrc_netdev->lwmac.awake_duration_sum_ticks) * 100 /
-//                   (duty - (uint64_t)gnrc_netdev->lwmac.system_start_time_ticks);
-//            printf("[LWMAC]: achieved duty-cycle: %lu %% \n", (uint32_t)duty);
-//#endif
-//            break;
-//        }
-//        case GNRC_LWMAC_SLEEPING: {
-//            gnrc_lwmac_clear_timeout(gnrc_netdev, GNRC_LWMAC_TIMEOUT_WAKEUP_PERIOD);
-//            break;
-//        }
-//        default:
-//            break;
-//    }
-//
-    /* Actions when entering new state */
-    switch (newstate) {
-//        /*********************** Operation states *********************************/
-//        case GNRC_LWMAC_LISTENING: {
-//            _gnrc_lwmac_set_netdev_state(gnrc_netdev, NETOPT_STATE_IDLE);
-//            break;
-//        }
-//        case GNRC_LWMAC_SLEEPING: {
-//            /* Put transceiver to sleep */
-//            _gnrc_lwmac_set_netdev_state(gnrc_netdev, NETOPT_STATE_SLEEP);
-//            /* We may have come here through RTT handler, so timeout may still be active */
-//            gnrc_lwmac_clear_timeout(gnrc_netdev, GNRC_LWMAC_TIMEOUT_WAKEUP_PERIOD);
-//
-//            if (gnrc_netdev_lwmac_get_phase_backoff(gnrc_netdev)) {
-//                gnrc_netdev_lwmac_set_phase_backoff(gnrc_netdev, false);
-//                uint32_t alarm;
-//
-//                rtt_clear_alarm();
-//                alarm = random_uint32_range(RTT_US_TO_TICKS((3 * GNRC_LWMAC_WAKEUP_DURATION_US / 2)),
-//                                            RTT_US_TO_TICKS(GNRC_LWMAC_WAKEUP_INTERVAL_US -
-//                                                            (3 * GNRC_LWMAC_WAKEUP_DURATION_US / 2)));
-//                LOG_WARNING("WARNING: [LWMAC] phase backoffed: %lu us\n", RTT_TICKS_TO_US(alarm));
-//                gnrc_netdev->lwmac.last_wakeup = gnrc_netdev->lwmac.last_wakeup + alarm;
-//                alarm = _next_inphase_event(gnrc_netdev->lwmac.last_wakeup,
-//                                            RTT_US_TO_TICKS(GNRC_LWMAC_WAKEUP_INTERVAL_US));
-//                rtt_set_alarm(alarm, rtt_cb, (void *) GNRC_LWMAC_EVENT_RTT_WAKEUP_PENDING);
-//            }
-//
-//            /* Return immediately, so no rescheduling */
-//            return;
-//        }
-//        /* Trying to send data */
-//        case GNRC_LWMAC_TRANSMITTING: {
-//            rtt_handler(GNRC_LWMAC_EVENT_RTT_PAUSE, gnrc_netdev);    /**< No duty cycling while RXing */
-//            _gnrc_lwmac_set_netdev_state(gnrc_netdev, NETOPT_STATE_IDLE);  /**< Power up netdev */
-//            break;
-//        }
-//        /* Receiving incoming data */
-//        case GNRC_LWMAC_RECEIVING: {
-//            rtt_handler(GNRC_LWMAC_EVENT_RTT_PAUSE, gnrc_netdev);    /**< No duty cycling while TXing */
-//            _gnrc_lwmac_set_netdev_state(gnrc_netdev, NETOPT_STATE_IDLE);  /**< Power up netdev */
-//            break;
-//        }
-//        case GNRC_LWMAC_STOPPED: {
-//            _gnrc_lwmac_set_netdev_state(gnrc_netdev, NETOPT_STATE_OFF);
-//            break;
-//        }
-        /*********************** Control states ***********************************/
-#if GNRC_CONTIKIMAC_RDC_ON
-        case GNRC_CONTIKIMAC_START:
-        		contikimac_set_netdev_state(gnrc_netdev, NETOPT_STATE_SLEEP);
-        		gnrc_netdev->contikimac.last_wakeup = rtt_get_counter();
-              rtt_handler(GNRC_CONTIKIMAC_EVENT_RTT_PERFORM_CCAPHASE, gnrc_netdev);
-//            lwmac_set_state(gnrc_netdev, GNRC_LWMAC_LISTENING);
-            break;
-#endif /* GNRC_CONTIKIMAC_RDC_ON */
-//        case GNRC_LWMAC_STOP: {
-//            rtt_handler(GNRC_LWMAC_EVENT_RTT_STOP, gnrc_netdev);
-//            lwmac_set_state(gnrc_netdev, GNRC_LWMAC_STOPPED);
-//            break;
-//        }
-//        case GNRC_LWMAC_RESET: {
-//            LOG_WARNING("WARNING: [LWMAC] Reset not yet implemented\n");
-//            lwmac_set_state(gnrc_netdev, GNRC_LWMAC_STOP);
-//            lwmac_set_state(gnrc_netdev, GNRC_LWMAC_START);
-//            break;
-//        }
-        /**************************************************************************/
-        default: {
-            LOG_DEBUG("[CONTIKIMAC] No actions for entering state %u\n", newstate);
-            return;
-        }
-    }
-//
-//    lwmac_schedule_update(gnrc_netdev);
-}
-
+/**
+ * @brief   RTT call back function.
+ *
+ */
 static void rtt_cb(void *arg)
 {
     msg_t msg;
@@ -292,20 +77,10 @@ static void rtt_cb(void *arg)
     msg.type = GNRC_CONTIKIMAC_EVENT_RTT_TYPE;
     msg_send(&msg, contikimac_pid);
 
-//    if (sched_context_switch_request) {
-//        thread_yield();
-//    }
+    if (sched_context_switch_request) {
+        thread_yield();
+    }
 }
-
-//void xtimer_handler(gnrc_contikimac_xtimer_t *type, gnrc_netdev_t *gnrc_netdev)
-//{
-//	switch (type) {
-//		case GNRC_CONTIKIMAC_XTIMER_SEND_STROBE:
-//			break;
-//	}
-//	
-//	return;
-//}
 
 void rtt_handler(uint32_t event, gnrc_netdev_t *gnrc_netdev)
 {
@@ -314,12 +89,11 @@ void rtt_handler(uint32_t event, gnrc_netdev_t *gnrc_netdev)
     switch (event & 0xffff) {
     	/* A new CCA phase starts. */
     	case GNRC_CONTIKIMAC_EVENT_RTT_PERFORM_CCAPHASE:
-    		//LED0_ON;
-    		gnrc_netdev->contikimac.last_wakeup = rtt_get_counter(); //rtt_get_alarm();
+    		gnrc_netdev->contikimac.last_wakeup = rtt_get_counter();
     		
     		if (gnrc_netdev->contikimac.state != GNRC_CONTIKIMAC_TRANSMITTING && // If we are transmitting, postpone cca phase. 
     				contikimac_perform_cca_phase(gnrc_netdev->dev)) { // A packet was detected.
-    			LED0_TOGGLE;
+    			LED0_TOGGLE; // LED0-0: Uncomment this line, if you want to toggle LED0 every time a packet was detected. Remember to comment out all other LED0-* lines.
     			contikimac_set_netdev_state(gnrc_netdev, NETOPT_STATE_IDLE);
     			alarm = rtt_get_counter() + GNRC_CONTIKIMAC_LISTEN_TIME_AFTER_PACKET_DETECTED;
     			rtt_set_alarm(alarm, rtt_cb, (void *) GNRC_CONTIKIMAC_EVENT_RTT_PACKET_DETECTED);
@@ -328,60 +102,15 @@ void rtt_handler(uint32_t event, gnrc_netdev_t *gnrc_netdev)
     			rtt_set_alarm(alarm, rtt_cb, (void *) GNRC_CONTIKIMAC_EVENT_RTT_PERFORM_CCAPHASE);
     		}
     		
-    		//LED0_OFF;
     		break;
     	/* After wait time after a packet was detected. */
     	case GNRC_CONTIKIMAC_EVENT_RTT_PACKET_DETECTED:
+    		// Schedule next CCA phase and let the radio sleep again.
     		alarm = gnrc_netdev->contikimac.last_wakeup + GNRC_CONTIKIMAC_CYCLE_TIME;
     		rtt_set_alarm(alarm, rtt_cb, (void *) GNRC_CONTIKIMAC_EVENT_RTT_PERFORM_CCAPHASE);
     		contikimac_set_netdev_state(gnrc_netdev, NETOPT_STATE_SLEEP);
     		break;
-//        case GNRC_LWMAC_EVENT_RTT_WAKEUP_PENDING: {
-//            /* A new cycle starts, set sleep timing and initialize related MAC-info flags. */
-//            gnrc_netdev->lwmac.last_wakeup = rtt_get_alarm();
-//            alarm = _next_inphase_event(gnrc_netdev->lwmac.last_wakeup,
-//                                        RTT_US_TO_TICKS(GNRC_LWMAC_WAKEUP_DURATION_US));
-//            rtt_set_alarm(alarm, rtt_cb, (void *) GNRC_LWMAC_EVENT_RTT_SLEEP_PENDING);
-//            gnrc_netdev_lwmac_set_quit_tx(gnrc_netdev, false);
-//            gnrc_netdev_lwmac_set_quit_rx(gnrc_netdev, false);
-//            gnrc_netdev_lwmac_set_phase_backoff(gnrc_netdev, false);
-//            gnrc_netdev->rx.rx_bad_exten_count = 0;
-//            lwmac_set_state(gnrc_netdev, GNRC_LWMAC_LISTENING);
-//            break;
-//        }
-//        case GNRC_LWMAC_EVENT_RTT_SLEEP_PENDING: {
-//            /* Set next wake-up timing. */
-//            alarm = _next_inphase_event(gnrc_netdev->lwmac.last_wakeup,
-//                                        RTT_US_TO_TICKS(GNRC_LWMAC_WAKEUP_INTERVAL_US));
-//            rtt_set_alarm(alarm, rtt_cb, (void *) GNRC_LWMAC_EVENT_RTT_WAKEUP_PENDING);
-//            lwmac_set_state(gnrc_netdev, GNRC_LWMAC_SLEEPING);
-//            break;
-//        }
-//        /* Set initial wake-up alarm that starts the cycle */
-//        case GNRC_LWMAC_EVENT_RTT_START: {
-//            LOG_DEBUG("[LWMAC] RTT: Initialize duty cycling\n");
-//            alarm = rtt_get_counter() + RTT_US_TO_TICKS(GNRC_LWMAC_WAKEUP_DURATION_US);
-//            rtt_set_alarm(alarm, rtt_cb, (void *) GNRC_LWMAC_EVENT_RTT_SLEEP_PENDING);
-//            gnrc_netdev_lwmac_set_dutycycle_active(gnrc_netdev, true);
-//            break;
-//        }
-//        case GNRC_LWMAC_EVENT_RTT_STOP:
-//        case GNRC_LWMAC_EVENT_RTT_PAUSE: {
-//            rtt_clear_alarm();
-//            LOG_DEBUG("[LWMAC] RTT: Stop duty cycling, now in state %u\n",
-//                           gnrc_netdev->lwmac.state);
-//            gnrc_netdev_lwmac_set_dutycycle_active(gnrc_netdev, false);
-//            break;
-//        }
-//        case GNRC_LWMAC_EVENT_RTT_RESUME: {
-//            LOG_DEBUG("[LWMAC] RTT: Resume duty cycling\n");
-//            rtt_clear_alarm();
-//            alarm = _next_inphase_event(gnrc_netdev->lwmac.last_wakeup,
-//                                        RTT_US_TO_TICKS(GNRC_LWMAC_WAKEUP_INTERVAL_US));
-//            rtt_set_alarm(alarm, rtt_cb, (void *) GNRC_LWMAC_EVENT_RTT_WAKEUP_PENDING);
-//            gnrc_netdev_lwmac_set_dutycycle_active(gnrc_netdev, true);
-//            break;
-//        }
+    		
         default:
             break;
     }
@@ -415,7 +144,6 @@ static void _event_cb(netdev_t *dev, netdev_event_t event)
 
                     if (pkt) {
                     	DEBUG("[CONTIKIMAC] Received packet of length %u.\n", gnrc_pkt_len(pkt));
-                    	//dev->contikimac.state = GNRC_CONTIKIMAC_RECEIVING;
                         _pass_on_packet(pkt);
                     }
 
@@ -423,7 +151,6 @@ static void _event_cb(netdev_t *dev, netdev_event_t event)
                 }
             case NETDEV_EVENT_TX_COMPLETE:
             	DEBUG("[CONTIKIMAC] A packet was successfully transmitted.\n");
-            	packet_received = true;
             	break;
 #ifdef MODULE_NETSTATS_L2
             case NETDEV_EVENT_TX_MEDIUM_BUSY:
@@ -500,17 +227,16 @@ static void *_contikimac_thread(void *args)
     /* Get own address from netdev */
     gnrc_netdev->l2_addr_len = dev->driver->get(dev, NETOPT_ADDRESS_LONG, &gnrc_netdev->l2_addr, IEEE802154_LONG_ADDRESS_LEN);
 
-//    assert(gnrc_netdev->l2_addr_len > 0);
+    assert(gnrc_netdev->l2_addr_len > 0);
 
     /* Initialize broadcast sequence number. This at least differs from board
      * to board */
-//    gnrc_netdev->tx.bcast_seqnr = gnrc_netdev->l2_addr[0];
+    //gnrc_netdev->tx.bcast_seqnr = gnrc_netdev->l2_addr[0];
 
-    /* Reset all timeouts just to be sure */
-//    gnrc_lwmac_reset_timeouts(gnrc_netdev);
-
-    /* Start duty cycling */
-    contikimac_set_state(gnrc_netdev, GNRC_CONTIKIMAC_START);
+    /* Put radio to sleep and schedule first CCA phase using RTT. */
+    contikimac_set_netdev_state(gnrc_netdev, NETOPT_STATE_SLEEP);
+	gnrc_netdev->contikimac.last_wakeup = rtt_get_counter();
+	rtt_handler(GNRC_CONTIKIMAC_EVENT_RTT_PERFORM_CCAPHASE, gnrc_netdev);
 	
     /* start the event loop */
     while (1) {
@@ -518,36 +244,18 @@ static void *_contikimac_thread(void *args)
 
         /* dispatch NETDEV and NETAPI messages */
         switch (msg.type) {
-        /* RTT raised an interrupt */
+        	/* RTT raised an interrupt */
 			case GNRC_CONTIKIMAC_EVENT_RTT_TYPE:
-				if (1 /*gnrc_netdev_lwmac_get_dutycycle_active(gnrc_netdev)*/) {
-					rtt_handler(msg.content.value, gnrc_netdev);
-//					lwmac_schedule_update(gnrc_netdev);
-				}
-				else {
-					LOG_DEBUG("[CONTIKIMAC] Ignoring late RTT event while dutycycling is off\n");
-				}
+				rtt_handler(msg.content.value, gnrc_netdev);
 				break;
-			/* An ContikiMAC timeout occured */
-			case GNRC_CONTIKIMAC_EVENT_SND_TYPE: {
-				DEBUG("[CONTIKIMAC] GNRC_CONTIKIMAC_EVENT_SND_TYPE received\n");
-				
-				gnrc_pktsnip_t *pkt = msg.content.ptr;
-				contikimac_send(gnrc_netdev, pkt);
-				
-				break;
-			}
             case NETDEV_MSG_TYPE_EVENT:
                 DEBUG("[CONTIKIMAC] GNRC_NETDEV_MSG_TYPE_EVENT received\n");
                 dev->driver->isr(dev);
                 break;
             case GNRC_NETAPI_MSG_TYPE_SND:
                 DEBUG("[CONTIKIMAC] GNRC_NETAPI_MSG_TYPE_SND received\n");
-                
-                if (gnrc_netdev->contikimac.state == GNRC_CONTIKIMAC_TRANSMITTING) break; // If we are still transmitting, just quit.
-                		
                 gnrc_pktsnip_t *pkt = msg.content.ptr;
-                contikimac_send(gnrc_netdev, pkt);
+                gnrc_netdev->send(gnrc_netdev, pkt);
                 break;
             case GNRC_NETAPI_MSG_TYPE_SET:
                 /* read incoming options */
